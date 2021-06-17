d67d9b346c0df6c4123625ba4305d87d901a910e
vankatwijk <hendrikus.hpvk@gmail.com>
2021-06-17 13:45:53 -0800

    jump logic and basic animation

---
+++ script.js
@@ -0,0 +1,11 @@
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+
+function jump() {
+    if (character.classList != "animate") {
+        character.classList.add("animate");
+    }
+    setInterval(function() {
+        character.classList.remove("animate");
+    }, 500)
+}
